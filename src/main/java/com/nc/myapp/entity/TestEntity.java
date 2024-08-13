242074d2955dd9e1738336d36871c763f7fa1eaf
NixonW <nixon1901@gmail.com>
2024-08-12 20:58:51 -0600

    Nombre del Proyecto: my-app. Descripcion: version final del proyecto my-app

---
+++ src/main/java/com/nc/myapp/entity/TestEntity.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.nc.myapp.entity;
+
+/**
+ * @author Nixon
+ *
+ */
+public class TestEntity {
+	private String nombre;
+	/**
+	 * Propiedad de prueba para la muestra de complilacion del archivo testentity
+	 */
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
